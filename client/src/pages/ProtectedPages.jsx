d6cc0bf596442704d835e351e287111dcf830b8c
souiiii <b423037@iiit-bh.ac.in>
2026-02-14 02:53:37 +0530

    ..

--- client/src/pages/ProtectedPages.jsx
+++ client/src/pages/ProtectedPages.jsx
@@ -1,13 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/useAuth";
-import Loading from "../components/Loading";
 import PageLoader from "../components/PageLoader";
 
 function ProtectedPages({}) {
   const values = useAuth();
   if (values.loading) {
     return <PageLoader show={true} />;
-
   } else if (values.user) {
     return <Outlet />;
   } else {
