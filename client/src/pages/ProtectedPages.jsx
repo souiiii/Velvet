b4230ac0d41135b9ea24b1e117ca5acc74936bd3
souiiii <b423037@iiit-bh.ac.in>
2026-02-14 02:54:22 +0530

    fixed object

--- client/src/pages/ProtectedPages.jsx
+++ client/src/pages/ProtectedPages.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/useAuth";
 import PageLoader from "../components/PageLoader";
 
-function ProtectedPages({}) {
+function ProtectedPages() {
   const values = useAuth();
   if (values.loading) {
     return <PageLoader show={true} />;
